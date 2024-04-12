a32a72a629bf132556a9ee8969cf9d84ef0be468
Tudor Gulin <gulintudor@gmail.com>
2024-04-12 18:58:00 +0300

    update

--- ProfessionalProfile/domain/Skill.cs
+++ ProfessionalProfile/domain/Skill.cs
@@ -10,13 +10,12 @@ namespace ProfessionalProfile.domain
     {
         private int _skillId;
         private string _name;
-        private string _skillLevel;
+        
 
-        public Skill(int skillId, string name, string skillLevel)
+        public Skill(int skillId, string name)
         {
             this._skillId = skillId;
             this._name = name;
-            this._skillLevel = skillLevel;
         }
 
         public int SkillId{
@@ -30,21 +29,13 @@ namespace ProfessionalProfile.domain
             set { _name = value; }
         }
 
-        public string SkillLevel
-        {
-            get { return _skillLevel; }
-            set { _skillLevel = value; }
-        }
-
         public override bool Equals(object? obj)
         {
             return obj is Skill skill &&
                    _skillId == skill._skillId &&
                    _name == skill._name &&
-                   _skillLevel == skill._skillLevel &&
                    SkillId == skill.SkillId &&
-                   Name == skill.Name &&
-                   SkillLevel == skill.SkillLevel;
+                   Name == skill.Name;
         }
     }
 }
