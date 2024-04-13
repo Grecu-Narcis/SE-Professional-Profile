bc2482dd6b5e70d3b00a0558ee5b1b94a08165dc
Goța Septimiu Andrei <111377955+AndreiGota@users.noreply.github.com>
2024-04-13 09:24:15 +0300

    added all functions for work experience repo

--- ProfessionalProfile/repo/WorkExperienceRepo.cs
+++ ProfessionalProfile/repo/WorkExperienceRepo.cs
@@ -35,7 +35,7 @@ namespace ProfessionalProfile.repo
                 command.Parameters.AddWithValue("@Responsibilities", item.Responsibilities);
                 command.Parameters.AddWithValue("@Achievements", item.Achievements);
                 command.Parameters.AddWithValue("@Description", item.Description);
-                command.Parameters.AddWithValue("@UserId", item.User_id);
+                command.Parameters.AddWithValue("@UserId", item.UserId);
 
                 command.ExecuteNonQuery();
             }
@@ -43,20 +43,116 @@ namespace ProfessionalProfile.repo
 
         public void Delete(int id)
         {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string sql = "EXEC DeleteWorkExperience @WorkId = @id";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                command.Parameters.AddWithValue("@id", id);
+
+                command.ExecuteNonQuery();
+            }
         }
 
         public List<WorkExperience> GetAll()
         {
-            throw new NotImplementedException();
+            List<WorkExperience> workExperiences = new List<WorkExperience>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string sql = "EXEC GetAllWorkExperiences";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int workId = (int)reader["WorkId"];
+                        string jobTitle = (string)reader["JobTitle"].ToString();
+                        string company = (string)reader["Company"].ToString();
+                        string location = (string)reader["Location"].ToString();
+                        string employementPeriod = (string)reader["EmployementPeriod"].ToString();
+                        string responsibilities = (string)reader["Responsibilities"].ToString();
+                        string achievements = (string)reader["Achievements"].ToString();
+                        string description = (string)reader["Description"].ToString();
+                        int userId = (int)reader["UserId"];
+
+                        WorkExperience workExperience = new WorkExperience(workId, userId, jobTitle, company, location, employementPeriod, responsibilities, achievements, description);
+                        workExperiences.Add(workExperience);
+                    }
+                }
+            }
+
+            return workExperiences;
         }
 
         public WorkExperience GetById(int id)
         {
-            throw new NotImplementedException();
+            WorkExperience workExperience = null;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string sql = "EXEC GetWorkExperienceById @WorkId = @id";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                command.Parameters.AddWithValue("@id", id);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            int workId = (int)reader["WorkId"];
+                            string jobTitle = (string)reader["JobTitle"].ToString();
+                            string company = (string)reader["Company"].ToString();
+                            string location = (string)reader["Location"].ToString();
+                            string employementPeriod = (string)reader["EmployementPeriod"].ToString();
+                            string responsibilities = (string)reader["Responsibilities"].ToString();
+                            string achievements = (string)reader["Achievements"].ToString();
+                            string description = (string)reader["Description"].ToString();
+                            int userId = (int)reader["UserId"];
+
+                            workExperience = new WorkExperience(workId, userId, jobTitle, company, location, employementPeriod, responsibilities, achievements, description);
+                        } 
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error getting work experience by id: {ex.Message}");
+                        }
+                        
+                    }
+                }
+            }
+            return workExperience;
         }
 
         public void Update(WorkExperience item)
         {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string sql = "EXEC UpdateWorkExperience @WorkId = @workId, @UserId = @userId, @JobTitle = @jobTitle, @Company = @company, @Location = @location, @EmployementPeriod = @employementPeriod, @Responsibilities = @responsibilities, @Achievements = @achievements, @Description = @description";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                command.Parameters.AddWithValue("@workId", item.WorkId);
+                command.Parameters.AddWithValue("@jobTitle", item.JobTitle);
+                command.Parameters.AddWithValue("@company", item.Company);
+                command.Parameters.AddWithValue("@location", item.Location);
+                command.Parameters.AddWithValue("@employementPeriod", item.EmployementPeriod);
+                command.Parameters.AddWithValue("@responsibilities", item.Responsibilities);
+                command.Parameters.AddWithValue("@achievements", item.Achievements);
+                command.Parameters.AddWithValue("@description", item.Description);
+                command.Parameters.AddWithValue("@userId", item.UserId);
+
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
