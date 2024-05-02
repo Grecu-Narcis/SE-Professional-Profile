d639501262aecfec829c798c26513aa1c809849f
dragosgavrus1 <dragos.gavrus@stud.ubbcluj.ro>
2024-05-02 17:53:50 +0300

    Refactored and Tested Entity Repo and Service

---
+++ Lab5/ISSProject/Iss/Repository/IAdRepository.cs
@@ -0,0 +1,19 @@
+﻿using Iss.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iss.Repository
+{
+    public interface IAdRepository
+    {
+        public void addAd(Ad ad);
+        public Ad getAdByName(string adName);
+        public List<Ad> getAdsThatAreNotInAdSet();
+        public List<Ad> getAdsForAdSet(string id);
+        public void updateAd(Ad ad);
+        public void deleteAd(Ad ad);
+    }
+}
